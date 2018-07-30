b4f9978dd2587635130c744a874fd0cd843395bf
DIOGO SOARES <diogo@i9me.com.br>
2018-07-30 15:00:49 -0300

    Adicionando o Footer

---
+++ template-parts/footer/section-footer-content.php
@@ -0,0 +1,51 @@
+<section class="Section Section--footerContent Section--style1 u-paddingVertical u-positionRelative u-displayFlex u-flexDirectionColumn u-flexJustifyContentCenter u-flexAlignItemsCenter">
+
+	<div class="Section-content u-positionRelative u-sizeFull u-flexAlignItemsCenter u-flexJustifyContentCenter">
+		<div class="u-positionAbsolute Section-content u-marginTop--inter u-positionAbsolute u-displayFlex u-size24of24 u-flexAlignItemsCenter u-flexJustifyContentCenter">
+			<ul class="Section-items u-displayFlex u-flexJustifyContentCenter u-positionAbsolute u-flexAlignItemsCenter u-sizeFull">
+				<li class="Section-items-item u-size8of24">
+					<i class="FigureIcon FigureIcon--newsletter"></i>
+					<div class="Section-items-item-content">
+						<p class="Section-items-item-resume u-alignCenter">/pizzavignoli</p>
+					</div>
+				</li>
+				<li class="Section-items-item u-size8of24">
+					<i class="FigureIcon FigureIcon--newsletter"></i>
+					<div class="Section-items-item-content">
+						<p class="Section-items-item-resume u-alignCenter">@pizzavignoli</p>
+					</div>
+				</li>
+				<li class="Section-items-item u-size8of24">
+					<i class="FigureIcon FigureIcon--newsletter"></i>
+					<div class="Section-items-item-content">
+						<p class="Section-items-item-resume u-alignCenter">/pizzavignoli</p>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="u-maxSize--container u-alignCenterBox u-displayFlex u-flexDirectionColumn u-flexSwitchRow">
+		<div class="Section-container u-displayFlex u-flexDirectionColumn u-flexAlignItemsCenter u-sizeFull u-paddingBottom">
+			<header class="Section-header u-marginBottom--inter u-alignCenterBox u-paddingVertical">
+			<h2 class="Section-header-title Section-header-title--beforeTitleLine u-alignCenter u-paddingBottom--inter--half u-marginBottom--inter--half">RECEBA AS NOSSAS <strong>NOVIDADES</strong></h2>
+		</header>
+			<div class="Section-content u-marginTop--inter u-size12of24">
+				<form class="Form Form--style3">
+					<fieldset class="Form-fieldset">
+						<div class="Form-line u-flex u-flexDirectionRow">
+							<div class="Form-row Form-row--email u-sizeFull u-positionRelative u-displayBlock">
+								<i class="u-icon u-icon--envelope">
+									<svg class="iconEnvelope u-icon is-animating">
+										<use xlink:href="#iconEnvelope"></use>
+									</svg>
+								</i>
+								<input class="Form-input Form-input--text u-borderRadius5 Form-border--0 u-size24of24" placeholder="Digite o seu e-mail" type="text" name="email" />
+								<input class="Form-input Form-input--submit FigureIcon--send is-animating hover" type="submit" value="" />
+							</div>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+		</div>
+	</div> <!-- Max Size Container -->
+</section>
