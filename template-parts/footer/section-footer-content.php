47f23257ae31480e343b6deaf9917cacf32fcbfa
Petrus Rennan <petrus@i9me.com.br>
2018-08-01 10:09:44 -0300

    master commit

---
+++ template-parts/footer/section-footer-content.php
@@ -0,0 +1,68 @@
+<section class="Section Section--footerContent Section--style1 u-paddingVertical u-positionRelative u-displayFlex u-flexDirectionColumn u-flexJustifyContentCenter u-flexAlignItemsCenter">
+
+	<div class="Section-content u-positionRelative u-sizeFull u-flexAlignItemsCenter u-flexJustifyContentCenter">
+		<div class="u-positionAbsolute Section-content u-marginTop--inter u-positionAbsolute u-displayFlex u-size24of24 u-flexAlignItemsCenter u-flexJustifyContentCenter">
+			<ul class="Section-items u-displayFlex u-flexJustifyContentCenter u-positionAbsolute u-flexAlignItemsCenter u-sizeFull">
+				<li class="Section-items-item u-size8of24">
+					<i class="FigureIcon FigureIcon--newsletter"></i>
+					<div class="Section-items-item-content">
+						<p class="Section-items-item-resume u-alignCenter">/pizzavignoli</p>
+					</div>
+				</li>
+				<li class="Section-items-item u-size8of24">
+					<i class="FigureIcon FigureIcon--newsletter"></i>
+					<div class="Section-items-item-content">
+						<p class="Section-items-item-resume u-alignCenter">@pizzavignoli</p>
+					</div>
+				</li>
+				<li class="Section-items-item u-size8of24">
+					<i class="FigureIcon FigureIcon--newsletter"></i>
+					<div class="Section-items-item-content">
+						<p class="Section-items-item-resume u-alignCenter">/pizzavignoli</p>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="u-displayFlex u-flexDirectionColumn u-flexSwitchRow u-sizeFull">
+		<div class="Section-container u-displayFlex u-flexDirectionColumn u-flexAlignItemsCenter u-sizeFull u-paddingBottom">
+			<header class="Section-header u-marginBottom--inter--half u-alignCenterBox u-paddingVertical">
+			<h2 class="Section-header-title Section-header-title--beforeTitleLine u-alignCenter u-paddingBottom--inter--half u-marginBottom--inter--half">RECEBA AS NOSSAS <strong>NOVIDADES</strong></h2>
+		</header>
+			<div class="Section-content u-size24of24 u-displayFlex u-flexDirectionColumn u-flexJustifyContentCenter">
+				<form class="Form Form--style3 u-displayFlex u-sizeFull">
+					<fieldset class="Form-fieldset u-displayFlex u-justifyContentCenter u-sizeFull">
+						<div class="Form-line u-displayFlex u-flexJustifyContentCenter">
+							<div class="Form-row Form-row--email u-positionRelative u-size8of24 u-boxShadow">
+							<div class="Form-content u-positionRelative">
+								<i class="u-icon u-icon--envelope">
+									<svg class="iconEnvelope u-icon is-animating">
+										<use xlink:href="#iconEnvelope"></use>
+									</svg>
+								</i>
+								<input class="Form-input Form-input--text Form-border--0" placeholder="Digite o seu e-mail" type="text" name="email" />
+							</div>
+							<div class="Form-content u-positionRelative">
+								<input class="Form-input Form-input--submit is-animating hover" type="submit" value="OK" />
+							</div>
+							</div>
+						</div>
+					</fieldset>
+				</form>
+				<figure class="Section-content u-displayFlex u-flexJustifyContentCenter u-paddingHorizontal--inter">
+					<img class="u-objectFitCover u-displayInlineBlock u-maxSize100 u-sizeHeight100 u-sizeAuto" src="<?php echo get_template_directory_uri() ?>/assets/images/vignologo--footer.png">
+				</figure>
+				<ul class="Section-items--contact">
+					<li class="Section-items-item--contact">
+						<i class="FigureIcon"></i>
+						<p>(85)3333-3333</p>
+					</li>
+					<li class="Section-items-item--contact ">
+						<i class="FigureIcon"></i>
+						<p>Rua xxxxxxxxxx, xxx, xxxxxxxxxx, xxxxxxx, XXXXX-XXX</p>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div> <!-- Max Size Container -->
+</section>
