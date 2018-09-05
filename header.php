2b788c767a2c46c95715f673eecc052e7149e2a4
DIOGO SOARES <diogo@i9me.com.br>
2018-09-05 11:21:38 -0300

    add Google Tag Manager

--- header.php
+++ header.php
@@ -19,11 +19,14 @@
   ?>
 
 <body <?php body_class(); ?>>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59J7428"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TNM849K');</script>
+<!-- End Google Tag Manager -->
 	
 <div id="page" class="Site">
 	<header id="masthead" class="Site-header u-positionAbsolute u-sizeFull u-paddingVertical u-paddingHorizontal--inter is-animating u-zIndex20">
